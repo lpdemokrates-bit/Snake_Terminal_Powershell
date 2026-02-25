360830dd872813f7dae79cd668b1632d316ff4a3
lpdemokrates-bit <lpdemokrates@gmail.com>
2026-02-25 12:57:28 +0100

    Add files via upload

--- SnakeGame.ps1
+++ SnakeGame.ps1
@@ -36,10 +36,23 @@ class GameBoard{
         $str_hor_d = "└" + $str_hor_d + "┘`r`n"
         $str_print = $str_hor_u + $str_mid + $str_hor_d
         Write-Host $str_print
+        Start-Sleep 1
     }
 
-    [void] update(){
+    [void] update($body){
         $this.clear()
+        for($i = 0; $i -lt $body.Count; $i++){
+            if($i -eq ($body.Count -1)){
+                $this.grid[$body[$i][1]][$body[$i][0]] = "<"
+              }
+            else{
+                $this.grid[$body[$i][1]][$body[$i][0]] = "a"
+            
+            }
+
+            # FOOD
+        
+        }
     }
 
 }
@@ -47,6 +60,7 @@ class GameBoard{
 
 
 class Snake{
+
     $snake_body
     $direction
     [boolean]$mov_pending
@@ -54,7 +68,10 @@ class Snake{
     [System.Collections.Queue]$key
 
     Snake([int] $x, [int] $y){
-        $this.snake_body = @(@(($x-1),$y),@($x,$y))
+        #$this.snake_body = @(@(($x-1),$y),@($x,$y))
+        $this.snake_body =  New-Object System.Collections.ArrayList
+        $this.snake_body.Add(@($x,$y))
+        $this.snake_body.Add(@(($x-1),$y))
         $this.direction = ([int]1,[int]0)
         $this.mov_pending = $true
         $this.score = 0
@@ -67,14 +84,14 @@ class Snake{
     }
 
     [void]mov([int]$size){
-        ## input translation
+        $this.input_trans()
         $new_head_pos = @(0,0)
         $new_head_pos[0] = (($this.snake_body[-1][0] + $this.direction[0]) % $size)
         $new_head_pos[1] = (($this.snake_body[-1][1] + $this.direction[1]) % $size)
         if ($this.mov_pending){
-            [System.Collections.ArrayList]$arrayList = $this.snake_body; $arrayList.RemoveAt(0)
+            $this.snake_body.RemoveAt(0)
             }
-        $this.snake_body += $new_head_pos
+        $this.snake_body.Add($new_head_pos)
         $this.mov_pending = $true
 
     }
@@ -84,9 +101,32 @@ class Snake{
         $this.score += 1
     
     }
+    [void]handle_input(){
+       while ($true){
+
+        if([Console]::KeyAvailable){
+         $this.key.Enqueue( [System.Console]::ReadKey($true).Key.ToString())
+    }
+            }
+     }
+     
+     [void]input_trans(){
+        $press = Read-Host 
+        switch ($press){
+            "w" {$this.direction = (0,-1)}
+            "s" {$this.direction = (0,1)}
+            "d" {$this.direction = (1,0)}
+            "a" {$this.direction = (-1,0)}
+                       
+        }
+        }
+        }
+     
+     
 
 
-}
+
+
 
 
 class GameEngine{
@@ -94,10 +134,40 @@ class GameEngine{
        [Snake] $snake
        [int] $size
        [boolean] $gamestate
-       
+       # Food
+       #?init Routine?
+       # THREADING
 
+       GameEngine([int] $size){
+            $this.board = [Gameboard]::new($size)
+            $x = [int] $size / 2
+            $y = [int] $size / 2
+            $this.snake = [Snake]::new($x,$y)
+            $this.size = $size
+            $this.gamestate = $true
+            
+        
+               
+       
+       }
+
+       [void]update_frames(){
+            $this.snake.mov($this.size)
+            ## Check hit
+            $this.board.update($this.snake.snake_body)
+            $this.board.display()
+       }
+
+       [void]run(){
+            while ($this.gamestate){
+                $this.update_frames()
+            }
+            
+       }                
 
 }
 
-$s = [Snake]::new(5,5)
-$s.mov(10)
+
+$game = [GameEngine]::new(10)
+
+$game.run()
